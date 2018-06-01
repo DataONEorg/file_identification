112297b833b1280dfd3cd4f159ef401eaff0afeb
pratikshrivastava <pratikshrivastava23@gmail.com>
2018-06-01 02:17:19 -0500

    modified:   regex

--- week1/python-libmagic/test_magic.py
+++ week1/python-libmagic/test_magic.py
@@ -3,7 +3,7 @@ from os import walk
 from os.path import isfile
 import magic
 import os
-
+import yaml
 
 mypath = "/Users/pratikshrivastava/Desktop/OneDrive - University of Illinois - Urbana/Box/GitHub/DataONE/internship2018/file_identification/examples"
 
@@ -15,7 +15,7 @@ fileObj = os.listdir(mypath)
 for obj in fileObj:
 	print(obj, isfile(obj))
 '''
-
+'''
 filetype= {}
 for root, dirs, files in os.walk(mypath):
 	for filename in files:
@@ -25,10 +25,35 @@ for root, dirs, files in os.walk(mypath):
 
 
 print(filetype)
-def chk_fil_type(magic_file, filename):
-	paths = [os.path.abspath('../magic_files/onedcx'), ]
+'''
+
+def getFileExt(folder, filename):
+	paths = [os.path.abspath('../magic_files/regex.mgc'), ]
 	flags = magic.MAGIC_MIME_TYPE | magic.MAGIC_MIME_ENCODING | magic.MAGIC_CONTINUE
 	flags=0
 	with magic.Magic(paths=paths, flags=flags) as m:
-	  res = m.id_filename("../magic_files/01_onedcx.xml")
-	  print("01_onedcx.xml:", res)
+	  res = m.id_filename(mypath +'/'+folder +'/'+filename)
+
+	return res
+	  #print(filename, res)
+
+def chkFileType(formatId, folder,list_obj):
+	for obj in list_obj:
+		fileName, fileExt = obj['filename'] , getFileExt(folder,obj['filename'])
+		#print("{0}\t{1}\t{2}".format(formatId, fileName, fileExt))
+		if formatId != fileExt:
+			print("{0}\t{1}\t{2}".format( fileName, formatId,fileExt))
+
+catalog_name = "../../examples/catalog.yaml"
+yaml_text = open(catalog_name, "r").read()
+catalog = yaml.load(yaml_text)
+filetype = {}
+
+print("*"*100)
+print("FileName\tFormatId\t\t\t\tFileExt")
+print("*"*100)
+print()
+for obj in catalog['examples']:
+	formatId = obj['formatId']
+	folder = obj["folder"]
+	chkFileType(formatId,folder, obj['identifiers'])
